3f58b008453ae2e582ae88cbc00f05be698aa959
CatalinBinzari <catalin.binzari99@gmail.com>
2020-07-10 15:54:28 +0300

    5.2

---
+++ yt5hrsTutorial/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<h1>hyyy</h1>, document.getElementById("root")) 
+//to render smth to thee screen
+//take 2 args: 1: what i want to render, 2:where do i want to render
